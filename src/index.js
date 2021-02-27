e74c7cbfb853d337af6f87b7eba55c77ed2fe5e9
Ivan Ilin <geniusmov@yandex.ru>
2021-02-27 14:59:23 +0300

    feat: implement min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length)
+    return 0;
+
+  let min = array[0];
+  array.forEach(element => {
+    if (element < min)
+      min = element
+  });
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length)
+    return 0;
+
+  let max = array[0];
+  array.forEach(element => { 
+    if (element > max)
+      max = element;
+  })
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length)
+    return 0;
+
+  let avg = 0;
+  array.forEach(element => {
+    avg += element;
+  })
+  return avg / array.length;
 }
